c25493085c6382ff1e5adfde6a1c5c21fb4b979d
Artem <32518639+MaksimovArtem@users.noreply.github.com>
2018-04-10 18:59:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,17 +15,6 @@ int main()
 	std::vector<double> all_prices;
 
 	//srand(time(0));
-	/*
-	double matrix[N][M] = { 1,3,2,2,8,1,5,7,
-	9,11,5,1,2,3,0,6,
-	5,7,4,9,15,3,5,4,
-	1,3,10,9,0,7,1,10,
-	6,0,7,2,8,3,2,6,
-	4,2,6,10,5,3,6,4,
-	8,13,1,4,0,6,8,0,
-	5,1,7,3,2,9,9,7};
-	*/
-
 	double matrix[N][M] = { 1,3,2,2,8,1,5,7,2,1,
 		9,11,5,1,2,3,0,6,3,2,
 		5,7,4,9,15,3,5,4,4,3,
@@ -59,8 +48,8 @@ int main()
 	}
 	*/
 	//Init
-	double searching_max_for_player_1[N]{};//íàêîï ñòð
-	double mixed_strategies_1[N]{};//âåêòîð ÷àñòîò
+	double searching_max_for_player_1[N]{};//Ð½Ð°ÐºÐ¾Ð¿ ÑÑ‚Ñ€
+	double mixed_strategies_1[N]{};//Ð²ÐµÐºÑ‚Ð¾Ñ€ Ñ‡Ð°ÑÑ‚Ð¾Ñ‚
 
 	double searching_min_for_player_2[M]{};
 	double mixed_strategies_2[M]{};
@@ -91,13 +80,17 @@ int main()
 	double lower_rating = 0;
 	double top_rating = 0;
 
-	
-	while (minimum_top_rating - maximum_lower_rating >= 2 * E)
-	//while (abs(price_on_the_last_turn - price_of_the_game) >= E)
+	int a, b;
+	//while (minimum_top_rating - maximum_lower_rating >= 2 * E)
+	while (abs(price_on_the_last_turn - price_of_the_game) >= E)
 	{
 		++game_turn;
 		price_on_the_last_turn = price_of_the_game;
-
+		if (game_turn != 1)
+		{
+			mixed_strategies_1[a]++;
+			mixed_strategies_2[b]++;
+		}
 		//start 1st player
 
 		for (int i = 0; i < N; i++)
@@ -122,25 +115,26 @@ int main()
 			searching_min_for_player_2[j] += matrix[last_strategy_of_player_1 - 1][j];
 		}
 
+
+		
 		double min_el = *std::min_element(searching_min_for_player_2, searching_min_for_player_2 + M);
 
-		for (int i = 0; i < N; i++)//ïîèñê ñëåäóþùåé ñòð èãð 1
+		for (int i = 0; i < N; i++)//Ð¿Ð¾Ð¸ÑÐº ÑÐ»ÐµÐ´ÑƒÑŽÑ‰ÐµÐ¹ ÑÑ‚Ñ€ Ð¸Ð³Ñ€ 1
 		{
 			if (searching_max_for_player_1[i] == max_el)
 			{
 				last_strategy_of_player_1 = i + 1;
-				mixed_strategies_1[i]++;
+				a = i;
 				break;
 			}
 		}
 
-		for (int j = 0; j < M; j++)//ïîèñê ñëåä ñòð 2
+		for (int j = 0; j < M; j++)//Ð¿Ð¾Ð¸ÑÐº ÑÐ»ÐµÐ´ ÑÑ‚Ñ€ 2
 		{
 			if (searching_min_for_player_2[j] == min_el)
 			{
 				last_strategy_of_player_2 = j + 1;
-				mixed_strategies_2[j]++;
-				//std::cout << "Player 2 uses " << j + 1 << " strategy" << std::endl;////////////////////////////
+				b = j;
 				break;
 			}
 		}
@@ -173,7 +167,7 @@ std::cout << "The lower rating is : " << lower_rating <<
 	std::cout << "Possibilities for player1:\n[ " << std::flush;
 	for (int i = 0; i < N; i++)
 	{
-		std::cout << mixed_strategies_1[i] / (game_turn + 1) << " " << std::flush;
+		std::cout << mixed_strategies_1[i] / (game_turn ) << " " << std::flush;
 	}
 
 	std::cout << "]\n Or: [ " << std::flush;
@@ -181,21 +175,21 @@ std::cout << "The lower rating is : " << lower_rating <<
 	{
 		std::cout << mixed_strategies_1[i] << " " << std::flush;
 	}
-	std::cout << "] for " << game_turn + 1 << " iter" << std::endl;
+	std::cout << "] for " << game_turn << " iter" << std::endl;
 
 
 	//
 	std::cout << "Possibilities for player2:\n[ " << std::flush;
 	for (int j = 0; j < M; j++)
 	{
-		std::cout << mixed_strategies_2[j] / (game_turn + 1) << " " << std::flush;
+		std::cout << mixed_strategies_2[j] / (game_turn ) << " " << std::flush;
 	}
 	std::cout << "]\n Or: [ " << std::flush;
 	for (int j = 0; j < M; j++)
 	{
 		std::cout << mixed_strategies_2[j] << " " << std::flush;
 	}
-	std::cout << "] for " << game_turn + 1 << " iter\n" << std::endl;
+	std::cout << "] for " << game_turn  << " iter\n" << std::endl;
 
 	/*
 	game_turn = 0;
